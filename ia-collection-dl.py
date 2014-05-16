e6cb8df8093ccec147a8d60903ad233778cb412e
Gregory Mullen (grayhatter) <greg@grayhatter.com>
2014-05-16 16:12:36 -0700

    move to functions

--- ia-collection-dl.py
+++ ia-collection-dl.py
@@ -7,29 +7,39 @@
 import sys
 import internetarchive as ia
 
-## Make sure the arguments are set
-if len(sys.argv) < 2:
-	print>>sys.stderr, "USAGE: " + sys.argv[0] + " $collectionName"
-	sys.exit(1)
 
-## Set/Print our collection name and search for it
-collection_name = 'collection:'+sys.argv[1]
-print "Attempting to download collection: " + sys.argv[1]
-collection = ia.Search(collection_name)
+def get_name():
+	## Make sure the arguments are set
+	if len(sys.argv) < 2:
+		sys.stderr.write("USAGE: " + sys.argv[0] + " <collectionName>\n")
+		sys.exit(1)
+	## Set/Print our collection name and search for it
+	return 'collection:'+sys.argv[1]
 
-## Really dirty total item count
-## Cant use len() on Search object
-current_item = 1
-total_item = 0
-for entry in collection:
-	total_item = total_item + 1
+def other(target):
+	print("Attempting to download collection: " + target)
+	collection = ia.Search(target)
 
-## Go through all items of the collection and download
-for entry in collection:
-	item_id = entry['identifier']
-	print 'Downloading ' + str(current_item) + "/" + str(total_item) + '\t' + item_id
+	## Really dirty total item count
+	## Cant use len() on Search object
+	current_item = 1
+	total_item = 0
+	for entry in collection:
+		total_item += 1
 
-	item = ia.Item(item_id)
-	status = item.download()
-	print '\t\t Download successful'
-	current_item = current_item + 1
+	## Go through all items of the collection and download
+	for entry in collection:
+		item_id = entry['identifier']
+		print('Downloading ' + str(current_item) + "/" + str(total_item) + '\t' + item_id)
+
+		item = ia.Item(item_id)
+		status = item.download()
+		print('\t\t Download successful')
+		current_item = current_item + 1
+
+
+def main():
+	name = get_name()
+
+if __name__ == '__main__':
+	main()
